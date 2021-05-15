6fe32f7ccd8decccfdae0e2ff12a13c8368cc048
Bharat Singh <vn0xvyq@UK.wal-mart.com>
2021-05-15 23:19:59 +0100

    Added Item Model

---
+++ PromotionEngine/ClassLibraryPromotionEngine/Models/Item.cs
@@ -0,0 +1,21 @@
+﻿namespace ClassLibraryPromotionEngine
+{
+    public class Item
+    {
+        public char SKU_Id { get; set; }
+        public int Quantity { get; set; }
+
+        public Item() { }
+
+        public Item(Item item)
+        {
+            SKU_Id = item.SKU_Id;
+            Quantity = item.Quantity;
+        }      
+
+        public override string ToString()
+        {
+            return $"{SKU_Id} {Quantity}";
+        }
+    }
+}
