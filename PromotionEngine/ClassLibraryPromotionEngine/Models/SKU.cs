f6d78fac8cb280208596711b3bb319723622ec6a
Bharat Singh <vn0xvyq@UK.wal-mart.com>
2021-05-15 23:17:54 +0100

    Added First Model Class SKU

--- PromotionEngine/ClassLibraryPromotionEngine/Models/SKU.cs
+++ PromotionEngine/ClassLibraryPromotionEngine/Models/SKU.cs
@@ -1,9 +1,15 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace ClassLibraryPromotionEngine
 {
     public class SKU
     {
         public char SKU_Id { get; set; }
-        public int Uni_tPrice { get; set; }
+        public int Unit_Price { get; set; }
     }
 }
+
--- PromotionEngine/PromotionEngineUnitTest/UnitTest1.cs
+++ PromotionEngine/PromotionEngineUnitTest/UnitTest1.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using ClassLibraryPromotionEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace PromotionEngineUnitTest
 {
     [TestClass]
     public class UnitTest1
     {
+        static readonly IEnumerable<SKU> lstSKU =
+            new List<SKU> 
+            {
+                new SKU { SKU_Id = 'A', Unit_Price = 50 },
+                new SKU { SKU_Id = 'B', Unit_Price = 30 },
+                new SKU { SKU_Id = 'C', Unit_Price = 20 },
+                new SKU { SKU_Id = 'D', Unit_Price = 15 }
+            };
+
         [TestMethod]
         public void TestMethod1()
         {
