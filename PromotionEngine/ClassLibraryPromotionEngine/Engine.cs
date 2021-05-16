9f7ea4040a8b8481d40839e82fe1a5247368e473
Bharat Singh <vn0xvyq@UK.wal-mart.com>
2021-05-16 10:45:41 +0100

    First Scenario completed

---
+++ PromotionEngine/ClassLibraryPromotionEngine/Engine.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassLibraryPromotionEngine
+{
+    public class Engine
+    {
+        private readonly IEnumerable<SKU> PriceList;
+        private readonly IEnumerable<Promotion> Promotions;
+
+        public Engine(IEnumerable<SKU> priceList, IEnumerable<Promotion> promotions)
+        {
+            this.PriceList = priceList;
+            this.Promotions = promotions;
+        }
+
+        public void CheckOut(Order order)
+        {
+            var foundItems = new List<Item>();
+            if (Promotions != null && Promotions.Count() > 0)
+                foreach (var promotion in Promotions)
+                {
+                    var validatedItems = promotion.Validate(order, foundItems);
+                    UpdateValidatedItems(foundItems, validatedItems);
+                }
+
+            ApplyRegularPrice(order, foundItems);
+        }
+
+        private void ApplyRegularPrice(Order order, List<Item> foundItems)
+        {
+            foreach (var item in order.Items)
+            {
+                var validateItem = foundItems.FirstOrDefault(x => x.SKU_Id == item.SKU_Id) ?? item;
+                var quantity = validateItem.Quantity;
+                if (quantity > 0)
+                    order.TotalAmount += quantity * PriceList.First(x => x.SKU_Id == item.SKU_Id).Unit_Price;
+            }
+        }
+
+        private static void UpdateValidatedItems(List<Item> foundItems, IEnumerable<Item> validatedItems)
+        {
+            if (validatedItems == null || validatedItems.Count() < 1)
+                return;
+
+            foreach (var item in validatedItems)
+                if (!foundItems.Any(x => x.SKU_Id == item.SKU_Id))
+                    foundItems.Add(item);
+        }
+    }
+}
--- PromotionEngine/PromotionEngineUnitTest/UnitTest1.cs
+++ PromotionEngine/PromotionEngineUnitTest/UnitTest1.cs
@@ -16,6 +16,39 @@ namespace PromotionEngineUnitTest
                 new SKU { SKU_Id = 'D', Unit_Price = 15 }
             };
 
+        static readonly IEnumerable<Promotion> Promotions =
+              new List<Promotion> 
+              {
+                    new Promotion 
+                    {
+                          Items = new List<Item> 
+                          {
+                            new Item { SKU_Id = 'A', Quantity = 3 }
+                          },
+                          TotalAmount = 130 
+                    },              // 3 of A for 130
+                    new Promotion 
+                    {
+                          Items = new List<Item> 
+                          {
+                            new Item { SKU_Id = 'B', Quantity = 2 }
+                          },
+                          TotalAmount = 45 
+                    },                // 2 of B for 45
+                    new Promotion 
+                    {
+                          Items = new List<Item> 
+                          {
+                            new Item { SKU_Id = 'C', Quantity = 1 },
+                            new Item { SKU_Id = 'D', Quantity = 1 }
+                          },
+                      TotalAmount = 30 
+                    }
+              };                        // C + D for 30
+        
+        static readonly Engine actualEngine = new Engine(lstSKU, Promotions);
+
+
         [TestMethod]
         public void Test_Scenario_A()
         {
@@ -28,7 +61,10 @@ namespace PromotionEngineUnitTest
                     new Item { SKU_Id = 'B', Quantity = 1 },
                     new Item { SKU_Id = 'C', Quantity = 1 }
                   }
-              };            
+              };
+
+            actualEngine.CheckOut(order);
+            Assert.IsTrue(order.TotalAmount == 100);
         }
     }
 }
